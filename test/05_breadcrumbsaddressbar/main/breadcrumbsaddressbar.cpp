6c3a10f3c06834edb616694298a6b5e6fa1ce485
iwanaga <iwanaga@azlab.co.jp>
2023-04-13 17:19:55 +0900

    test/05, update CMakeLists, breadcrumbsaddressbar

--- test/05_breadcrumbsaddressbar/main/breadcrumbsaddressbar.cpp
+++ test/05_breadcrumbsaddressbar/main/breadcrumbsaddressbar.cpp
@@ -2,16 +2,23 @@
 #include "breadcrumbsaddressbar.h"
 
 #include <QSize>
+#include <QStyleFactory>
+#include <QApplication>
 
 const QSize TRANSPARENT_ICON_SIZE = QSize(40, 40);
 
 BreadCrumbsAddressBar::BreadCrumbsAddressBar(QWidget* parent, Qt::WindowFlags f)
   : QFrame(parent, f)
+  , m_styleProxy(nullptr)
 {
-  
+  m_styleProxy = new StyleProxy(QPixmap(":/icons/assets/arrow-right_128x128.png"), QStyleFactory::create(qApp->style()->objectName()));
 }
 
 BreadCrumbsAddressBar::~BreadCrumbsAddressBar()
 {
-  
+  if (m_styleProxy)
+  {
+    delete m_styleProxy;
+    m_styleProxy = nullptr;
+  }
 }
--- test/05_breadcrumbsaddressbar/main/breadcrumbsaddressbar.h
+++ test/05_breadcrumbsaddressbar/main/breadcrumbsaddressbar.h
@@ -3,12 +3,16 @@
 #define BREAD_CRUMBS_ADDRESS_BAR_H_
 
 #include <QFrame>
+#include "styleproxy.h"
 
 class BreadCrumbsAddressBar : public QFrame
 {
 public:
   explicit BreadCrumbsAddressBar(QWidget *parent = nullptr, Qt::WindowFlags f = Qt::WindowFlags());
   virtual ~BreadCrumbsAddressBar();
+
+private:
+  StyleProxy* m_styleProxy;
 };
 
 #endif // BREAD_CRUMBS_ADDRESS_BAR_H_
